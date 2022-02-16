3644011f4853569f115bf3bba1b74b26f34ffbc5
kosuke-17 <tamutamu17@icloud.com>
2022-02-16 18:12:41 +0900

    プロックファイル作成

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { typeDefs } from "./typeDefs";
 
 async function StartApolloServer() {
   const app = express();
-  const PORT = 4000 || process.env.PORT;
+  const PORT = process.env.PORT || 4000;
 
   /**
    * ApolloServerをインスタンス化
